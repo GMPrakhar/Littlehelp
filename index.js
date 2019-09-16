2160b67fa975724fa694b5efa047b9bc2f9ef4dd
Prakhar <prakhar.londhe30@gmail.com>
2019-09-16 23:38:54 +0530

    Added all the required google drive upload, view, admin panel view/delete/addfolder functionality.

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
+var formidable = require('formidable');
+const { google } = require('googleapis')
+const key = require("./credentials.json");
+const scopes = 'https://www.googleapis.com/auth/drive';
+const jwt = new google.auth.JWT(
+  key.client_email,
+  null,
+  key.private_key,
+  scopes
+);
+let drive = google.drive('v3');
+
+
+
+
+
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -22,8 +39,228 @@ app.get('/study', function(request, response) {
   response.render('pages/study');
 });
 
+
+      
+app.post('/uploadFile', function(req, res){
+  var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+      var parents = [];
+      if(fields.sem != "any") parents.push(fields.sem);
+      if(fields.branch != "any") parents.push(fields.branch);
+      if(fields.type != "any") parents.push(fields.type);
+      console.log(parents);
+      var fileMetadata = {
+        'name': files.filetoupload.name,
+        parents: parents,
+        properties: {sem: fields.actsem, branch: fields.actbr, type: fields.acttype, uploader: "Littlehelp Team"}
+        
+      };
+      var media = {
+        mimeType: files.filetoupload.type,
+        body: fs.createReadStream(files.filetoupload.path)
+      };
+
+
+      jwt.authorize((err, response) => {
+       /* google.drive('v3').files.list(
+          {
+            auth: jwt,
+          },
+          (err, result) => {
+            console.log(err);
+            console.log(result.data.files);
+          }
+        );
+      */
+        
+      drive.files.create({
+        auth: jwt,
+        resource: fileMetadata,
+        media: media,
+        fields: 'id'
+      }, function (err2, file) {
+        if (err2) {
+          // Handle error
+          console.error(err2);
+        } else {
+          console.log('Uploaded File Id: ', file);
+        }
+      });
+      });
+
+
+
+      res.write('File uploaded');
+      res.end();
+    });
+});
+
+
+app.get('/getResults', function(req,res){
+  var parents = " and";
+  var fields = req.query;
+      if(fields.sem != 'undefined' && fields.sem != "any"){
+        parents += " '"+fields.sem + "' in parents ";
+      }
+      if(fields.branch != 'undefined' && fields.branch != "any"){
+        if(parents != " and") parents += "and ";
+        parents += " '"+fields.branch + "' in parents ";
+      }
+      if(fields.type != 'undefined' && fields.type != "any"){
+        if(parents != " and") parents += "and ";
+        parents += " '"+fields.type + "' in parents ";
+      }
+      console.log(parents);
+      if(parents==" and") parents = "";
+  jwt.authorize((err, response) => {
+    google.drive('v3').files.list(
+      {
+        auth: jwt,
+        q: "mimeType!='application/vnd.google-apps.folder' " + parents,
+        fields: "files(name, id, properties, createdTime, webViewLink)"
+      },
+      (err, result) => {
+        if(err) {
+          console.log(err);
+          res.end('Some error occured');
+        }
+        else
+        res.end(JSON.stringify({files: result.data.files}));
+      }
+    );
+  });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
+
+
+
+
+
+
+
+
+//-------------------------Admin Panel---------------------------
+
+app.get('/admin', function(request, response) {
+  if(request.query.pass == "@Humteen3"){
+    response.render("pages/admin");
+  }else{
+    response.end("Invalid Passcode entered!");
+  }
+  
+});
+
+app.get('/fileList', function(req, res){
+  var parent = req.query.parent?req.query.parent:'root';
+  jwt.authorize((err, response) => {
+    google.drive('v3').files.list(
+      {
+        auth: jwt,
+        q: "mimeType!='application/vnd.google-apps.folder' and '"+parent+"' in parents"
+      },
+      (err, result) => {
+        if(err) res.end(err);
+        else
+        res.end(JSON.stringify({files: result.data.files}));
+      }
+    );
+  });
+});
+
+app.get('/folderList', function(req, res){
+  var parent = req.query.parent?req.query.parent:'root';
+  jwt.authorize((err, response) => {
+    google.drive('v3').files.list(
+      {
+        auth: jwt,
+        q: "mimeType='application/vnd.google-apps.folder' and '"+parent+"' in parents"
+      },
+      (err, result) => {
+        console.log(err);
+        if(err) res.end(err);
+        else
+        res.end(JSON.stringify({folders: result.data.files}));
+      }
+    );
+  });
+});
+
+app.get('/enableViewPermissions', function(req, res){
+  var permissions = 
+    {
+      'role': 'reader',
+      'type': 'anyone'
+    }
+  ;
+  jwt.authorize((err, response) => {
+    google.drive('v3').permissions.create({
+      auth: jwt,
+      resource: permissions,
+      fileId: '1ibYfFvJwMSlBOgfnRfRJgLycExpLnsNk',
+      fields: 'id'
+    }, function(err, resp){
+      if(err) console.log(err);
+      else res.end("Permission id:" + resp.id);
+    });
+});
+});
+
+
+app.get('/createFolder', function(req, res){
+  var name = req.query.name;
+  var parent = req.query.parent?req.query.parent:'root';
+  var fileMetadata = {
+    name: name,
+    mimeType: "application/vnd.google-apps.folder",
+    parents: [parent]
+  };jwt.authorize((err, response) => {
+  drive.files.create({
+    auth: jwt,
+    resource: fileMetadata,
+    fields: 'id',
+  }, function (err2, file) {
+    if (err2) {
+      // Handle error
+      console.error(err2);
+    } else {
+      console.log('Created Id: ', file);
+      res.end(file.id);
+    }
+  });
+  });
+});
+
+
+app.get('/deleteFile', function(req, res){
+  var id = req.query.id;
+  jwt.authorize((err, response) => {
+  drive.files.delete({
+    auth: jwt,
+    fileId: id 
+  }, function (err2, resp) {
+    if (err2) {
+      // Handle error
+      console.error(err2);
+      res.end("Some error occured");
+    } else {
+      console.log('Deleted Id: ');
+      res.end("Deleted file  ");
+    }
+  });
+  });
+});
+
+
+
+
+
+
+
+
+
+
 
 
