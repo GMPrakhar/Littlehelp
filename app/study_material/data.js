437e1ef82d18a132eeaea96779053b8ef68fd6b4
Prakhar Londhe <plondhe@vmware.com>
2020-04-05 17:26:18 +0530

    Added the mocha tests to tests directory to be directly run by `npm test`

--- app/study_material/data.js
+++ app/study_material/data.js
@@ -1,9 +1,11 @@
 let key = require("../../credentials.json");
 
-key.private_key_id = process.env.drive_private_key_id;
-key.private_key = process.env.drive_private_key.replace(/\\n/g, '\n');
-key.client_id = process.env.drive_client_id;
-
+if(process.env != undefined)
+{
+    key.private_key_id = process.env.drive_private_key_id;
+    key.private_key = process.env.drive_private_key.replace(/\\n/g, '\n');
+    key.client_id = process.env.drive_client_id;
+}
 const { google } = require('googleapis');
 const scopes = 'https://www.googleapis.com/auth/drive';
 const jwt = new google.auth.JWT(
--- tests/index/test.js
+++ tests/index/test.js
@@ -4,15 +4,20 @@ var chaiHttp = require('chai-http');
 var expect = chai.expect;
 var should = chai.should();
 
+var environment = require('dotenv');
+environment.config();
+
 chai.use(chaiHttp);
 
-var index = require('./index');
+var index = require('../../index');
 
 describe('Routing', function () {
   
   it('should redirect to https if request is http', function (done) {
+    console.log(process.env.environment);
       chai.request('http://localhost:5000').get('/').then(function(res){
-        expect(res).to.redirect;
+        if(process.env.environment == "Production") expect(res).to.redirect;
+        else expect(res).status(200);
         done();
       }).catch(done);
   }); 
